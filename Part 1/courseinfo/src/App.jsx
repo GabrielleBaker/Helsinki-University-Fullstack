c05aaf4a38445ad4f606a3627fa5ee027b47c367
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-18 19:53:14 +0300

    Exercise 1.2
    Exercise 1.2

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -13,7 +13,13 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content contents={contents}/>
+      {
+     // exercise 1.1
+     //   <Content contents={contents}/>
+      }
+      <Content part1={part1} exercises1={exercises1} 
+              part2={part2} exercises2={exercises2} 
+              part3={part3} exercises3={exercises3} />
       <Total contents={contents}/>
     </div>
   ) 
@@ -27,7 +33,8 @@ const Header = ({course})=>{
     </h1>
     )
 }
-
+//exercise 1.1
+/*
 const Content = ({contents})=>{
 
   return(
@@ -45,13 +52,34 @@ const Content = ({contents})=>{
       </div>
     
   )
+}*/
+
+//exercise 1.2
+const Content = (props)=>{
+
+  return(
+    <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={ props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = ({part,exercises})=>{
+
+  return(
+    <p>
+        {part} {exercises}
+    </p>
+  )
 }
 
 const Total =({contents})=>{
   //map exercises to new array
   const exerciseArray = contents.map((item) => item.exercises);
   let sum=0;
-  
+
   //for each loop through and add it to the sum
   exerciseArray.forEach((exercise)=>{
     sum+=exercise;
