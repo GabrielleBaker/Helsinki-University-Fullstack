fd12ff1436348c2abf232e4e0d911eeadd2780eb
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-22 17:40:37 +0300

    Exercise 1.3

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -1,5 +1,6 @@
 const App = () => {
   const course = 'Half Stack application development'
+  /* exercise 1.1 and 1.2
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -9,20 +10,31 @@ const App = () => {
   const contents = [{part: part1,exercises: exercises1},
                     {part: part2,exercises: exercises2},
                     {part: part3,exercises: exercises3}]
+*/
+//exercise 1.3
+const part1 = {
+  name: 'Fundamentals of React',
+  exercises: 10
+}
+const part2 = {
+  name: 'Using props to pass data',
+  exercises: 7
+}
+const part3 = {
+  name: 'State of a component',
+  exercises: 14
+}
 
-  return (
-    <div>
-      <Header course={course}/>
-      {
-     // exercise 1.1
-     //   <Content contents={contents}/>
-      }
-      <Content part1={part1} exercises1={exercises1} 
-              part2={part2} exercises2={exercises2} 
-              part3={part3} exercises3={exercises3} />
-      <Total contents={contents}/>
-    </div>
-  ) 
+
+return (
+  <div>
+    <Header course={course}/>
+    <Content part={part1}/>
+    <Content part={part2}/>
+    <Content part={part3}/>
+    <Total part1={part1} part2={part2} part3={part3}/>
+  </div>
+); 
 }
 
 
@@ -55,27 +67,30 @@ const Content = ({contents})=>{
 }*/
 
 //exercise 1.2
+ /*
 const Content = (props)=>{
 
   return(
     <div>
+     exercise 1.1 and 1.2
     <Part part={props.part1} exercises={props.exercises1} />
     <Part part={props.part2} exercises={props.exercises2}/>
     <Part part={props.part3} exercises={ props.exercises3}/>
+    
     </div>
   )
-}
+}*/
 
-const Part = ({part,exercises})=>{
+/*const Part = ({part,exercises})=>{
 
   return(
     <p>
         {part} {exercises}
     </p>
   )
-}
+}*/
 
-const Total =({contents})=>{
+/*const Total =({contents})=>{
   //map exercises to new array
   const exerciseArray = contents.map((item) => item.exercises);
   let sum=0;
@@ -91,6 +106,24 @@ const Total =({contents})=>{
     </div>
 
   )
+}*/
+
+//exercise 1.3
+const Content = ({ part }) => {
+  return (
+    <div>
+      {part.name} {part.exercises}
+    </div>
+  );
+}
+//exercise 1.3
+const Total = ({ part1, part2, part3 }) => {
+  const sum = part1.exercises + part2.exercises + part3.exercises;
+  return (
+    <div>
+      <p>Total number of exercises {sum}</p>
+    </div>
+  );
 }
 
 export default App
