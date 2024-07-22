1e79f517a7b55bfad904869fa44cf0f148e62b2f
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-22 17:52:51 +0300

    Exercise 1.5

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -1,26 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-
+//exercise 1.5
 return (
   <div>
-    <Header course={course}/>
-    <Content parts={parts} />
-    <Total parts={parts} />
+    <Header course={course.name}/>
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 ); 
 }
@@ -34,7 +36,7 @@ const Header = ({course})=>{
     )
 }
 
-//exercise 1.4
+
 const Content = ({parts})=>{
 
   return(
