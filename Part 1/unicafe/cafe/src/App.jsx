260342fe8ed97a7c00ba11c14c4c2a5f89473a5f
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-24 15:39:20 +0300

    Exercise 1.7
    1.7: unicafe step 2

--- Part 1/unicafe/cafe/src/App.jsx
+++ Part 1/unicafe/cafe/src/App.jsx
@@ -12,27 +12,44 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll]=useState(0);
+  
 
+  const setGoodTo = () => {
+    setGood(good+1) 
+    setAll(all+1)
+  }
+  // 1.6 could also be written as const setGoodTo = () => setGood(good+1)
+ 
+  const setNeutralTo = () => {
+    setNeutral(neutral+1) 
+    setAll(all+1)
+  }
 
-  const setGoodTo = () => setGood(good+1);
-  /*could also be written as 
-  const setGoodTo = () => { 
-    return (
-    setGood(good+1)
-    ) 
-    }
-  */
-  const setNeutralTo = () => setNeutral(neutral+1);
-  const setBadTo = () => setBad(bad+1);  
+  const setBadTo = () => {
+    setBad(bad+1)
+    setAll(all+1)
+  }
 
   //title sections component
   const Titles =({text})=><h1>{text}</h1>
 
   //statistics component
   const Stats = () => {
+    
+    let average ;
+    if (all !==0 ){
+        average= (good - bad) / all;}
+        else {average =0;}
+
+    let positive ;
+    if(all !== 0){
+       positive = (good / all) * 100;}
+       else {positive =0;}
+
     return(
       <p>
       Good: {good} 
@@ -40,7 +57,14 @@ const App = () => {
       Neutral: {neutral} 
       <br></br>
       Bad: {bad}
+      <br></br>
+      All: {all}
+      <br></br>
+      Average: {average} 
+      <br></br>
+      Positive: {positive} %
       </p>
+
     )
   }
 
