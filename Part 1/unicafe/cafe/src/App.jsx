59bf8aaa513fb9228af2796ac09aad029467b0d6
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-24 22:41:35 +0300

    Exercise 1.11
    1.11*: unicafe step 6
    Change StatsLine component to return table rows and data
    Change Stats component to incl an array of data, return table and table body and feed array data to statsLine to return table rows and data

--- Part 1/unicafe/cafe/src/App.jsx
+++ Part 1/unicafe/cafe/src/App.jsx
@@ -13,9 +13,19 @@ import { useState } from 'react'
  //title sections component
  const Titles =({text})=><h1>{text}</h1>
 
+// statistic line component exercise 1.11
+//return as table data instead of text
+ const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
 //statistics component exercise 1.8
 const Stats = ({good, bad, neutral, all}) => {
-
+  let average ;
+  let positive ;
   //message if no feedback given yet
   //exercise 1.9
   if(all===0){
@@ -26,37 +36,41 @@ const Stats = ({good, bad, neutral, all}) => {
   //once feedback has been given
   //exercise 1.7 more stats
   else{
-    let average ;
+   
     if (all !==0 ){
         average= (good - bad) / all;}
         else {average =0;}
   
-    let positive ;
+   
     if(all !== 0){
        positive = (good / all) * 100;}
        else {positive =0;}
+
+  //1.11 using array of data to feed to StatisticLine to create table
+  const statsArray=  [
+      {text:'Good:', value:good} ,
+      {text:'Neutral:', value:neutral} ,
+      {text:'Bad:', value:bad},
+      {text:'All:', value:all} ,
+      {text:'Average:', value:average},
+      {text:'Positive:', value:positive}
+]
   
     return(
-      <div>
-      <StatisticLine text='Good:' value={good}/> 
-      <StatisticLine text='Neutral:' value={neutral}/> 
-      <StatisticLine text='Bad:' value={bad}/> 
-      <StatisticLine text='All:' value={all}/> 
-      <StatisticLine text='Average:' value={average}/>
-      <StatisticLine text='Positive:' value={positive}/>
-      </div>
-  
+        <table>
+          {
+          //exercise 1.11
+          //return a table, then use StatisticLine component to create table rows from
+          //the array of data statsArray    
+          }
+          <tbody>
+            {statsArray.map((stat,index)=>(
+              <StatisticLine key={index} text={stat.text} value={stat.value}/>
+            ))}
+          </tbody>
+        </table>
     )
   }
-}
-// statistic line component exercise 1.10
-const StatisticLine = ({text,value})=>{
-return(
-  <>
-    {text} {value}
-    <br></br>
-  </>
-)
 }
 
 const App = () => {
