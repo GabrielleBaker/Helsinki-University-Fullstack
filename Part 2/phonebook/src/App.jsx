af2c593ab3b6339276ab7fcb1c26326d7d8b8e34
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-29 17:02:32 +0300

    Exercise 2.7
    2.7: The Phonebook Step 2
    Check if a name already exists (used find method) and if so create an alert & do not save name to list

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,51 +1,63 @@
-import { useState } from 'react'
-import Person from './components/Person'
-
+import { useState } from 'react';
+import Person from './components/Person';
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
-
+  ]);
+  const [newName, setNewName] = useState('');
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const personObject = {
       name: newName
+    };
+
+    //check if person already exists using find. find method returns first match from array
+    //object reference equality doesnt factor when using find.name
+    //if using find person ===personobject => wont work as expected bc its checking memory location
+    //but using find person.name and personobject.name compares the value of properties not the 
+    //objects themselves
+    //if a match is not found, returns undefined 
+    const existingPerson = persons.find(person => person.name === personObject.name);
+    
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+      //console.log('person exists');
+    } else {
+     // console.log('adding new person');
+      setPersons(persons.concat(personObject));
     }
-  
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
+
+    setNewName('');
+  };
 
   const handlePersonChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-        name: <input
+          name: <input
             value={newName}
-          onChange={handlePersonChange}
-        />
+            onChange={handlePersonChange}
+          />
         </div>
         <div>
-        <button type="submit">add</button>
+          <button type="submit">add</button>
         </div>
-      </form> 
+      </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
           <Person key={person.name} person={person} />
         )}
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
