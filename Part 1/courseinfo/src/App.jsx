d020b1412a81bc541063c684fb262d958186ec00
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-29 13:56:43 +0300

    Exercise 2.4
    2.4: Course information step 9
    Let's extend our application to allow for an arbitrary number of courses.
    Added course title, and then subsequent program titles. changed course component to map through programs

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -1,4 +1,19 @@
-const Header = ({course}) => <h1>{course}</h1>
+//gives title as h2
+const Header = ({courses}) => {
+  return(
+    <h2>
+      {courses}
+    </h2> 
+    )
+}
+//use for h1 titles
+const Titles = ({title})=>{
+  return(
+    <h1>
+    {title}
+    </h1>
+    )
+}
 
 const Total =({parts})=>{
   //reduce iterates through array adding each item value to the sum
@@ -17,54 +32,98 @@ const Total =({parts})=>{
       </>
 
   const Content = ({parts}) => {
+  //map through each part of the parts array of each program using each
+  //part id as key
+  //use the part's data (name and exercises) to create a Part from
+  // the part component and return it as an html paragraph 
     return(
       <div>
-        {parts.map((part)=>
+      {parts.map((part) => (
         <p key={part.id}>
-          <Part part={part}/>
+          <Part part={part} />
         </p>
-        )}
-        </div>
+      ))}
+    </div>
       
     )
   }
  
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
 return (
   <div>
-  <Header course={course.name}/>
-  <Content parts={course.parts}/>
-  <Total parts={course.parts}/>
+    <Titles title={courses.title}/>
+    {
+//map through the array of programs in the courses, using program id as the key
+//for each program array
+//create a header from the program name, then create a content object
+//from the content component using the program parts as the parts prop
+    }
+    {courses.programs.map((program) => (
+    <div key={program.id}>
+    <Header courses={program.name}/>
+    <Content  parts={program.parts} />
+    <Total parts={program.parts}/>
+    </div>
+      ))}
+    
   </div>
 )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = 
+  { title: 'Web Development Curriculum',
+    programs:
+    [{
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]}
 
-    return <Course course={course} />
-  
+    return( 
+    <div>
+    <Course courses={courses}/>
+    </div>
+    )
 }
 
 export default App
