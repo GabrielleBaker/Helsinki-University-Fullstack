e4dcd8d15d517109a34409cbdee1003366ac59d3
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2024-07-30 11:23:34 +0300

    Exercise 2.9
    2.9*: The Phonebook Step 4
    Implement a search field to show filtered results of persons

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -3,16 +3,36 @@ import Person from './components/Person';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]);
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber]=useState('');
+  const [showAll, setShowAll] = useState(true);
+  const [searchName,setSearchName]=useState('');
+
+
+  // if persons exists to filter, create new array of persons to show ie filtered people
+  //filter the persons array and add all people that contain whatever searchName consists of
+  //search updates as typing in input field occurs
+  //toLowerCase ensures case insensitivity 
+  const filteredPersons= showAll
+  ? persons
+  : persons.filter(person=>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
 
   const addPerson = (event) => {
     event.preventDefault();
+    //create new person object from input fields
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length+1
     };
 
     //check if person already exists using find. find method returns first match from array
@@ -27,7 +47,7 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       //console.log('person exists');
     } else {
-     // console.log('adding new person');
+     //console.log(personObject.id);
       setPersons(persons.concat(personObject));
     }
 
@@ -42,10 +62,22 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  //once typing in the search input field occurs, the showall sets to false, and starts
+  //displaying the filtered persons array instead of all persons
+  const handleSearchChange = (event)=>{
+    setSearchName(event.target.value);
+    setShowAll(false);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          Filter search: <input
+            value={searchName}
+            onChange={handleSearchChange}
+          />
+        </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -62,15 +94,13 @@ const App = () => {
           </div>
         
         <div>
-          <button type="submit">add</button>
+          <button type="submit"> add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
+      {filteredPersons.map(person => 
+          <Person key={person.id} person={person} />
         )}
-      </div>
     </div>
   );
 };
